dc0c0b448922dafd75d5fa20acd857bf07275a9a
aquafox111 <w4xff69@gmail.com>
2026-06-06 10:24:32 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,9 +1,9 @@
 
 
 module.exports = {
-    token: process.env.TOKEN,
+    token: process.env.TOKEN,MTUxMjM2OTY0MTQ5NjkwNzgyNw.GIR2EK.mPFb-0ECXr_wzJ6gH1mz0x13inWCnCN54LfWTE
     prefix: process.env.PREFIX || '!',
-    clientId: process.env.CLIENT_ID,
+    clientId: process.env.CLIENT_ID,1512369641496907827
     guildId: process.env.GUILD_ID,
     owners: ['1312309380800057355'], // Add bot owner IDs
     colors: {
